4a475ef5c49b66ca56ce6b50f2b963ce26df3e58
Rajesh Mondal <rajesh01749@gmail.com>
2023-10-12 16:44:33 +0600

    task manager functionality done using php oop

---
+++ Module-4 (Object Oriented Programing)/Project 1 (Task Manager)/app/Classes/DeadlineTask.php
@@ -0,0 +1,38 @@
+<?php
+namespace app\Classes;
+
+use app\Classes\Task;
+use app\Traits\HasAssigneeTrait;
+use app\Interfaces\HasDueDateInterface;
+use app\Interfaces\HasStartDateInterface;
+
+class DeadlineTask extends Task implements HasDueDateInterface, HasStartDateInterface {
+    use HasAssigneeTrait;
+
+    protected $startDate;
+    protected $dueDate;
+
+    //Methods
+    public function __construct( $title, $description, $startDate, $dueDate ) {
+        parent::__construct( $title, $description );
+        $this->startDate = $startDate;
+        $this->dueDate = $dueDate;
+    }
+
+    //Getters
+    public function getStartDate() {
+        return $this->startDate;
+    }
+
+    public function getDueDate() {
+        return $this->dueDate;
+    }
+
+    public function getPriority() {
+        return "Low";
+    }
+
+    public function markAsCompleted() {
+        $this->completed = true;
+    }
+}
