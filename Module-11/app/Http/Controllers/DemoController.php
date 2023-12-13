a267ad150d653f3b27f1a6a011961de4b34dc8f0
Rajesh Mondal <rajesh01749@gmail.com>
2023-12-13 17:12:26 +0600

    order by, having, group by, latest oldest, take skip practices

--- Module-11/app/Http/Controllers/DemoController.php
+++ Module-11/app/Http/Controllers/DemoController.php
@@ -84,4 +84,85 @@ class DemoController extends Controller {
         $otherQuery = DB::table( 'products' )->where( 'products.discount', '=', 1 )->union( $query )->get();
         return $otherQuery;
     }
+
+    function whereClause() {
+        $result = DB::table( 'products' )
+            ->where( 'products.price', '>=', 2000 )
+            // ->where( 'products.title', 'LIKE', '%Sho%' )
+            ->get();
+        return $result;
+    }
+
+    function advanceWhere() {
+        $result = DB::table( 'products' )
+            ->where( 'products.price', '>', 2000 )
+            ->orWhere( 'products.price', '=', 1000 )
+            // ->whereNot( 'products.price', '=', 1000 )
+            // ->whereBetween( 'price', [1, 1500] )
+            ->get();
+        return $result;
+    }
+
+    function whereNull() {
+        $result = DB::table( 'products' )
+            ->whereNull( 'price' )
+            // ->whereNotNull( 'price' )
+            ->get();
+        return $result;
+    }
+
+    function whereIn() {
+        $result = DB::table( 'products' )
+            // ->whereIn( 'price', [1000, 5000] )
+            ->whereNotIn( 'products.price', [1000, 5000] )
+            ->get();
+        return $result;
+    }
+
+    function whereDateTime() {
+        $result = DB::table( 'brands' )
+            ->whereDate( 'created_at', '2023-02-19')
+            // ->whereMonth( 'created_at', '02')
+            // ->whereDay( 'created_at', '19')
+            // ->whereYear( 'created_at', '2023')
+            // ->whereTime( 'created_at', '20:05:15')
+            ->get();
+        return $result;
+    }
+
+    function whereColumn() {
+        $result = DB::table( 'brands' )
+            ->whereColumn( 'created_at', '<', 'updated_at')
+            ->get();
+        return $result;
+    }
+
+    function orderBy() {
+        $result = DB::table( 'brands' )->orderBy( 'brandName', 'desc' )->get();
+        // Random Order
+        // $result = DB::table( 'brands' )->inRandomOrder()->first();
+        return $result;
+    }
+
+    function latestOldest() {
+        // $affected = DB::table( 'brands' )->latest()->first();
+        $affected = DB::table( 'brands' )->oldest()->first();
+        return $affected;
+    }
+
+    function skipTake() {
+        $affected = DB::table( 'products' )
+            ->skip(2)
+            ->take(3)
+            ->get();
+        return $affected;
+    }
+
+    function having() {
+        $affected = DB::table( 'products' )
+            ->groupBy( 'price' )
+            ->having( 'price', '>', 2000 )
+            ->get();
+        return $affected;
+    }
 }
