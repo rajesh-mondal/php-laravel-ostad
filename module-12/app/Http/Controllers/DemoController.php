d111042214fbd75ecdeb6664cd09f872c2d810db
Rajesh Mondal <rajesh01749@gmail.com>
2023-12-20 14:08:22 +0600

    eloquent orm (retriveing all rows, columns, aggregation, select, increment-decrement)

--- module-12/app/Http/Controllers/DemoController.php
+++ module-12/app/Http/Controllers/DemoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\Brand;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class DemoController extends Controller
@@ -23,6 +24,40 @@ class DemoController extends Controller
 
     public function delete( Request $request ) {
         return Brand::where('id', '=', $request->id)->delete();
+    }
+
+    public function increDecrement( Request $request ) {
+        return Product::where( 'id', 1 )->increment( 'price', 100 );
+        // return Product::where( 'id', 1 )->increment( 'price' );
+        // return Product::where( 'id', 1 )->decrement( 'price', 100 );
+    }
+
+    public function getAll( Request $request ) {
+        // return Brand::get();
+        return Brand::all();
+    }
+
+    public function singleRow( Request $request ) {
+        return Brand::first();
+        // return Brand::find( 2 );
+    }
+
+    public function columnList( Request $request ) {
+        // return Product::pluck( 'price' );
+        return Product::pluck( 'price', 'title' );
+    }
+
+    public function aggregate( Request $request ) {
+        return Product::sum( 'price' );
+        // return Product::avg( 'price' );
+        // return Product::max( 'price' );
+        // return Product::min( 'price' );
+        // return Product::count( 'price' );
+    }
+
+    public function select( Request $request ) {
+        // return Product::select( 'title', 'price', 'star' )->get();
+        return Product::select( 'title' )->distinct()->get();
     }
 
 }
